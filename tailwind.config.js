70b67f56c950a846acb79be5d226b80b59ef4d0e
amyepes <amyepes@uninorte.edu.co>
2026-04-30 19:59:45 -0500

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+export default {
+  theme: {
+    extend: {
+      colors: {
+        'red-color': '#e94560',
+        'blue-color': '#20c6de',
+        'sub-text-color': '#6e6e78',
+      },
+      backgroundImage: {
+        'gradient-color': 'linear-gradient(158deg, rgba(17, 26, 44, 1) 0%, rgba(27, 37, 55, 1) 50%, rgba(15, 24, 43, 1) 100%)',
+      },
+    },
+  },
+  plugins: [],
+}
